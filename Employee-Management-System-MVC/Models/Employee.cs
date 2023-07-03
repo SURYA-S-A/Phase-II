f6dc71ad1af51f60b37387182f73708f2e0c4340
SURYA <105117742+SURYA-S-A@users.noreply.github.com>
2023-07-03 23:27:51 +0530

    Add files via upload

---
+++ Employee-Management-System-MVC/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Employee_Management_System_MVC.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        [Required(ErrorMessage = "Please enter the employee name")]
+        public string EmployeeName { get; set; }
+
+        [Required(ErrorMessage = "Please select the employee gender")]
+        public char EmployeeGender { get; set; }
+
+        public DateTime EmployeeBirthDate { get; set; }
+
+        public int FK_PositionId { get; set; }
+
+        public decimal EmployeeSalary { get; set; }
+
+        public string EmployeeMobileNumber { get; set; }
+
+        public int EmployeeAge { get; set; }
+
+        public string PositionName { get; set; }
+
+        public string EmployeeImageUrl { get; set; }
+
+    }
+}
